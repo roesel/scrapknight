b8b627310f62508c262958a747009bc36c28f5d8
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-11-30 23:57:47 +0100

    Use mysql.connector

--- database.py
+++ database.py
@@ -1,29 +1,25 @@
-from config_nonflask import *
-import MySQLdb
+from config_nonflask import config
 
+import mysql.connector
 
 class Database:
-    host = HOST
-    user = USER
-    password = PASSWORD
-    db = DB
 
     def __init__(self):
-        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
-        self.cursor = self.connection.cursor()
+
+        self.cnx = mysql.connector.connect(**config)
+        self.cursor = self.cnx.cursor()
 
     def insert(self, query):
         try:
             self.cursor.execute(query)
-            self.connection.commit()
+            self.cnx.commit()
         except:
-            self.connection.rollback()
+            self.cnx.rollback()
 
     def query(self, query):
-        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(query)
+        self.cursor.execute(query)
 
-        return cursor.fetchall()
+        return self.cursor.fetchall()
 
     def __del__(self):
-        self.connection.close()
+        self.cnx.close()
