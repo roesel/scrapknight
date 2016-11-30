6f11bca80c4ccefcbb973106b517d6b4dba47a7d
David Roesel <roesel@gmail.com>
2016-11-30 22:09:49 +0100

    Changed import to remove DeprecationWarning

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
-class InputForm(Form):
+
+class InputForm(FlaskForm):
     #openid = StringField('openid', validators=[DataRequired()])
     #remember_me = BooleanField('remember_me', default=False)
     text = TextAreaField('input', default='')
