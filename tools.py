80804b25650bf6a076ad3b75f1261c8562d791c5
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-11-30 20:39:57 +0100

    Introduce classes Deck and Card, port functionality from "process".

--- tools.py
+++ tools.py
@@ -5,7 +5,7 @@ import numpy as np
 import urllib.request
 import flask
 
-def process(input):
+def process(user_input):
     matches = []
     for limit in np.arange(0, 300, 30):
         url = 'http://cernyrytir.cz/index.php3?akce=3&limit='+str(limit)+'&edice_magic=KLD&poczob=100&foil=R&triditpodle=ceny&hledej_pouze_magic=1&submit=Vyhledej'
@@ -21,48 +21,94 @@ def process(input):
 
     db = {}
     for a in matches:
-        if ( (str.find(a[0], ' - lightly played') == -1) and (str.find(a[0], '- foil') == -1) ):
+        if ((str.find(a[0], ' - lightly played') == -1) and (str.find(a[0], '- foil') == -1)):
             #print(a[0]+" "+a[1])
             db[a[0].lower()] = {'cost': int(a[1]), 'title': a[0]}
 
-    #print(db)
+    Card.db = db
 
-    found_all = True
-    price = 0
-    out = ''
-    outable = []
-    for row in input.splitlines():
-        split = row.replace('\n', '').split(' x ')
-        if ( (len(split) == 2) and (row[0] != '#') ):
-            count = int(split[0])
-            name = split[1]
-            id = name.lower()
-            
-            if id in db:
-                ppu = db[id]['cost']
-                title = db[id]['title']
-                
-                out += title + " - " + str(count) + " x " + str(ppu) + " CZK" + flask.Markup('<br />')
-                multiprice = count * ppu
-                outable.append([title, str(count), str(ppu), str(multiprice)])
+    mydeck = Deck(user_input)
+
+    return mydeck.print_price_table()
+
+
+class Deck():
+
+    def __init__(self, user_input):
+        """
+        """
+        self.cards = []
+
+        for i, row in enumerate(user_input.splitlines()):
+
+            split = row.replace('\n', '').split(' x ')
+
+            if not row[0] == '#':
+                if len(split) == 2:
+                    count = int(split[0])
+                    name = split[1]
+
+                    card = Card(name.lower())
+
+                    card.count = count
+                    card.user_input = name
+
+                    self.cards.append(card)
+
+                else:
+                    raise ValueError(
+                        "Error while processing input row {}: {}".format(i, row))
+
+    def print_price_table(self):
+        """
+        """
+
+        found_all = True
+        price = 0
+        table = []
+
+        for card in self.cards:
+
+            if card.found:
+                multiprice = card.count * card.cost
+                table.append([
+                    str(card.title),
+                    str(card.count),
+                    str(card.cost),
+                    str(multiprice)])
                 price += multiprice
+
             else:
                 found_all = False
-                title = name
-                out += name+" - "+str(count)+" x ? (card not found)"+flask.Markup('<br />')
-                outable.append([title, str(count), "?", "?"])
-                
-    out += '----------------'+flask.Markup('<br />')
-    footer = []
-    success = False
-    if not found_all:
-        out += "Didn't find all cards!!!"+flask.Markup('<br />')
-        footer.append(["Minimum price - some cards not found", "", "", str(price)+" CZK"])
-    else:
-        footer.append(["Full price", "", "",  str(price)+" CZK"])
-        success = True
-    out += "Full price: "+str(price)+" CZK."+flask.Markup('<br />')
-    
-    header = ["Card title", "Count", "PPU [CZK]", "Price [CZK]"]
-    
-    return header, outable, footer, success
+                table.append([
+                    str(card.user_input), # tady bacha na nejaky user injection
+                    str(card.count),
+                    '?',
+                    '?'])
+
+        footer = []
+
+        if not found_all:
+            footer.append(["Minimum price - some cards not found", "", "", str(price) + " CZK"])
+        else:
+            footer.append(["Full price", "", "",  str(price) + " CZK"])
+
+        header = ["Card title", "Count", "PPU [CZK]", "Price [CZK]"]
+
+        return header, table, footer, found_all
+
+class Card():
+
+    def __init__(self, card_id):
+        """
+        """
+
+        self.card_id = card_id
+
+        if card_id in self.db:
+            self.found = True
+            self.cost = self.db[card_id]['cost']
+            self.title = self.db[card_id]['title']
+
+        else:
+            self.found = False
