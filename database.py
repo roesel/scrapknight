ec12bbcf8c7081511f29680b210e143761d371c0
David Roesel <roesel@gmail.com>
2016-11-30 22:28:16 +0100

    Split Database() object to pieces.

---
+++ database.py
@@ -0,0 +1,29 @@
+from config_nonflask import *
+import MySQLdb
+
+
+class Database:
+    host = HOST
+    user = USER
+    password = PASSWORD
+    db = DB
+
+    def __init__(self):
+        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        self.cursor = self.connection.cursor()
+
+    def insert(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+
+    def query(self, query):
+        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(query)
+
+        return cursor.fetchall()
+
+    def __del__(self):
+        self.connection.close()
--- fill_db.py
+++ fill_db.py
@@ -5,34 +5,7 @@ import numpy as np
 import urllib.request
 import time
 import warnings
-import MySQLdb
-
-
-class Database:
-    host = 'localhost'
-    user = 'root'
-    password = ''
-    db = 'scrapknight'
-
-    def __init__(self):
-        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
-        self.cursor = self.connection.cursor()
-
-    def insert(self, query):
-        try:
-            self.cursor.execute(query)
-            self.connection.commit()
-        except:
-            self.connection.rollback()
-
-    def query(self, query):
-        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(query)
-
-        return cursor.fetchall()
-
-    def __del__(self):
-        self.connection.close()
+from database import *
 
 
 class Scraper:
