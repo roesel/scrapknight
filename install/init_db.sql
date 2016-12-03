8c82288e74f7ea322e0cfa49c80c5395568f6296
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-12-03 11:44:21 +0100

    Fix db init - order of table creation

--- install/init_db.sql
+++ install/init_db.sql
@@ -7,6 +7,15 @@ DROP DATABASE IF EXISTS `scrapknight`;
 CREATE DATABASE `scrapknight` /*!40100 DEFAULT CHARACTER SET utf32 COLLATE utf32_czech_ci */;
 USE `scrapknight`;
 
+DROP TABLE IF EXISTS `editions`;
+CREATE TABLE `editions` (
+  `id` varchar(50) COLLATE utf32_czech_ci NOT NULL COMMENT 'ID edice (XXX...)',
+  `name` varchar(50) COLLATE utf32_czech_ci DEFAULT NULL COMMENT 'Plný název edice',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_czech_ci COMMENT='Seznam zkratek edic a jejich plných názvů.';
+
+
 DROP TABLE IF EXISTS `cards`;
 CREATE TABLE `cards` (
   `id` varchar(20) COLLATE utf32_czech_ci NOT NULL COMMENT 'ID karty',
@@ -37,15 +46,6 @@ CREATE TABLE `costs` (
   PRIMARY KEY (`card_id`),
   CONSTRAINT `costs_ibfk_3` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_czech_ci COMMENT='Různé ceny jednotlivých karet.';
-
-
-DROP TABLE IF EXISTS `editions`;
-CREATE TABLE `editions` (
-  `id` varchar(50) COLLATE utf32_czech_ci NOT NULL COMMENT 'ID edice (XXX...)',
-  `name` varchar(50) COLLATE utf32_czech_ci DEFAULT NULL COMMENT 'Plný název edice',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_czech_ci COMMENT='Seznam zkratek edic a jejich plných názvů.';
 
 
 DROP TABLE IF EXISTS `card_details`;
