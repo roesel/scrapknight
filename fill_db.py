fcfa194426dbaf0432dad5c3fa833f49cf43e6f2
David Roesel <roesel@gmail.com>
2016-12-06 17:00:40 +0100

    [Style] Minor comment for fill_db.

--- fill_db.py
+++ fill_db.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+This script creates a new Scraper object and rebuilds the database according to the methods called.
+The object is then thrown away, so any results must be stored elsewhere to persist.
+"""
+
 from scraper import Scraper
 
 sc = Scraper()
