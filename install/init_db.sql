39563a1effb22f8c8620fb651998e1a4ed1218bd
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-12-06 15:02:34 +0100

    Remove COLLATE (not exactly sure why)

--- install/init_db.sql
+++ install/init_db.sql
@@ -9,20 +9,20 @@ USE `scrapknight`;
 
 DROP TABLE IF EXISTS `editions`;
 CREATE TABLE `editions` (
-  `id` varchar(50) COLLATE utf8_general_ci NOT NULL COMMENT 'ID edice (XXX...)',
-  `name` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Plný název edice',
+  `id` varchar(50) NOT NULL COMMENT 'ID edice (XXX...)',
+  `name` varchar(50) DEFAULT NULL COMMENT 'Plný název edice',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Seznam zkratek edic a jejich plných názvů.';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Seznam zkratek edic a jejich plných názvů.';
 
 
 DROP TABLE IF EXISTS `cards`;
 CREATE TABLE `cards` (
-  `id` varchar(20) COLLATE utf8_general_ci NOT NULL COMMENT 'ID karty',
-  `name` varchar(100) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Název karty',
-  `edition_id` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Edice karty (zdratka)',
-  `manacost` varchar(10) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Manacost (formát?)',
-  `md5` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'MD5 hash jména karty',
+  `id` varchar(20) NOT NULL COMMENT 'ID karty',
+  `name` varchar(100) DEFAULT NULL COMMENT 'Název karty',
+  `edition_id` varchar(50) DEFAULT NULL COMMENT 'Edice karty (zkratka)',
+  `manacost` varchar(10) DEFAULT NULL COMMENT 'Manacost (formát?)',
+  `md5` varchar(50) DEFAULT NULL COMMENT 'MD5 hash jména karty',
   PRIMARY KEY (`id`),
   KEY `edition_id` (`edition_id`),
   KEY `name` (`name`),
@@ -30,19 +30,19 @@ CREATE TABLE `cards` (
   KEY `md5` (`md5`),
   FULLTEXT `name_fulltext` (`name`),
   CONSTRAINT `cards_ibfk_2` FOREIGN KEY (`edition_id`) REFERENCES `editions` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Seznam všech karet v databázi.';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Seznam všech karet v databázi.';
 
 
 DROP TABLE IF EXISTS `costs`;
 CREATE TABLE `costs` (
-  `card_id` varchar(20) COLLATE utf8_general_ci NOT NULL COMMENT 'ID karty',
+  `card_id` varchar(20) NOT NULL COMMENT 'ID karty',
   `buy` smallint(5) unsigned DEFAULT NULL,
   `buy_foil` smallint(5) unsigned DEFAULT NULL,
   `sell` smallint(5) unsigned DEFAULT NULL,
   `sell_foil` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`card_id`),
   CONSTRAINT `costs_ibfk_3` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Různé ceny jednotlivých karet.';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Různé ceny jednotlivých karet.';
 
 
 DROP VIEW IF EXISTS `card_details`;
